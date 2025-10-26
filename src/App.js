8f1f028f15a83307a6ff58a82017e4173a1275d1
Mohammed Shahid ul Islam <omgsshahid15@gmail.com>
2025-10-26 20:39:08 +0530

    just changed picture, experimented with hover styling , added some raw events data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+const LogoFD = require('./LogoFD.jpg');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={LogoFD} className="App-logo" alt="LogoFD" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Startup Summit 2025
+          
+          📅 Date: 2nd November 2025
+          📍 Venue: T-Hub, Hyderabad
+          🕒 Time: 10:00 AM – 6:00 PM
         </p>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hyderabad Entrepreneurship Summit (HES 2025)– Powered by TiE Hyderabad 🌟
+          
+        📅 Date: November 1st, 2025
+        📍 Venue: Hitex - Hall 4 Hyderabad
+        💰 Student Pass: ₹399/-(only for today)
         </p>
+
+        <p>
+          🚀 Big Opportunity Alert!
+TBI, KLU Vijayawada is visiting KLH Aziz Nagar to support and fund the brightest ideas on campus.
+• Existing Startups: 1 Investment Round
+• Projects: 2 Evaluation Rounds
+
+📝 The registration form will close on Monday, 27th October.
+🎤 Pitching will be conducted on Tuesday, 28th October.
+        </p>
+
+        <p>oracle ki present free Certification  unnai fast ga complete chesukondi <a href ="https://education.oracle.com/race-to-certification-2025">link to oracle free certificaion</a></p>
+        <p> opportunity for creative students: KLH invites applications for in-house student designers</p>
         <a
           className="App-link"
           href="https://reactjs.org"
